390c905bad1730be699c0bbd7ca195efa637d461
Phy Llova <phyllova@acelinkcourier.com>
2023-12-02 23:37:00 -0800

    added new changes to address and email

---
+++ secured/users/btc.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+<title>Cryptocurrency Payment</title>
+ <link rel="SHORTCUT ICON" href="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png">
+<style>
+.abf-frame p {
+	font-family: Helvetica!important;
+	line-height: 18px;
+	margin: 0!important;
+	padding: 0!important
+}
+.abf-frame {
+	color: #000!important;
+	background-color: #fff!important;
+	position: absolute;
+	top: calc(50% - 245px);
+	left: calc(50% - 180px);
+	font-family: Helvetica!important;
+	width: 400px;
+	max-height: 600px;
+	margin-top: 0;
+	text-align: left;
+	box-shadow: 0 12px 28px rgba(0,0,0,0.1);
+	border-radius: 3px;
+	font-size: 12px
+}
+.abf-frame a {
+	font-family: Helvetica!important;
+	color: #6A8FC2!important
+}
+.abf-frame a:hover {
+	color: #676573!important
+}
+.abf-form {
+	padding: 0 24px 24px
+}
+.abf-header {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 5px 24px;
+	min-height: 93px
+}
+.abf-header div:nth-child(1) img {
+	display: inline-block;
+	margin: 5px 0
+}
+.abf-ash1 {
+	text-align: center;
+	font-size: 14px;
+	margin: 12px 0
+}
+.abf-ash2 {
+	font-size: 14px;
+	text-align: center;
+	margin: 12px 0;
+	font-weight: 700
+}
+.abf-ash3 {
+	font-size: 14px;
+	text-align: center;
+	margin: 12px 0;
+	font-weight: 700
+}
+.abf-topline {
+	border-top: 1px solid #dedede!important;
+	padding-top: 12px
+}
+.abf-list-item {
+	padding: 4px 0;
+	display: flex;
+	align-items: baseline
+}
+.abf-label {
+	display: inline-block;
+	width: 45%;
+	padding-right: 24px;
+	box-sizing: border-box;
+	vertical-align: top;
+	font-size: 12px;
+	opacity: .5;
+	text-align: right
+}
+.abf-value {
+	display: inline-block;
+	width: 48%;
+	box-sizing: border-box
+}
+.abf-confirmations {
+	display: inline-block;
+	background-color: #dc3545!important;
+	width: 12px;
+	height: 12px;
+	font-size: 9px;
+	line-height: 12px;
+	text-align: center;
+	color: #fff!important;
+	border-radius: 50%;
+	margin-left: 3px
+}
+.abf-green {
+	background-color: #28a745!important
+}
+.abf-img-height {
+	max-height: 80px
+}
+</style>
+<div class="abf-frame">
+    <div class="abf-address abf-topline abf-ash3 abf-input-address">Cryptocurrency Payment</div>
+  <div class="abf-header">
+
+    <div>
+      <div class="abf-ash1"><img src="https://image.shutterstock.com/image-photo/image-260nw-767986060.jpg" width="95" alt=""></div>
+    </div>
+
+    <div style="text-align: center; background-color:#fff;"><span class="abf-qr"> <img class="abf-img-height" src="https://logowik.com/content/uploads/images/tether-usdt3962.jpg" style="display: inline;" alt="QR code for payment"> </span> </div>
+  </div>
+  <div class="abf-form">
+    <div class="abf-ash1"> Please send Minimum <strong><span class="abf-totalbtc">50</span></strong> USD
+      to address: </div>
+    <div class="abf-address abf-topline abf-ash2 abf-input-address">BITCOIN : bc1q9lrsqvjq4g6zrwrwdxmmn5nmhp95zc6jghnra0</div>
+    <div class="abf-address abf-topline abf-ash2 abf-input-address">USDT(TRC-20) : TVNu5MkK6SRhJbdTYjimLLMhoLmrVpn9z6</div>
+    <div class="abf-address abf-topline abf-ash2 abf-input-address">USDT(ERC-20) : 0xDCdeD0C9Ca6d39673683FEAF79057699b2065A59</div>
+    <div class="abf-address abf-topline abf-ash2 abf-input-address">ETH : 0xDCdeD0C9Ca6d39673683FEAF79057699b2065A59</div>
+    <div class="abf-data abf-topline">
+      <div class="abf-list">
+        <div class="abf-list-item">
+
+        </div>
+        <div class="abf-list-item">
+          <div class="abf-label">Minimum to pay:</div>
+          <div class="abf-value"><span class="abf-totalbtc">50</span> USD</div>
+        </div>
+        </div>
+        </div>
+
+        <div class="abf-list-item abf-tx-block">
+          <div class="abf-label">Transaction(s):</div>
+          <div class="abf-value abf-tx">
+            <div>Cryptocurrency
+              <div class="abf-confirmations abf-green" title="Confirmations count">2</div>
+            </div>
+          </div>
+        </div>
+        <div class="abf-list-item">
+          <div class="abf-label">Status:</div>
+          <div class="abf-value"><b><span class="abf-status">Waiting payment</span></b></div>
+        
+		</div>
+		 <div class="abf-list-item">
+          <div class="abf-label">Note:</div>
+          <div class="abf-value"><span class="abf-arrived">Your account will be filled after we confirm your transaction</span></div>
+        </div>
+		<center><br><br><br><br><input type="button" value="Go Back" onclick="history.back(-1)" />
+      </div>
+    </div>
+  </div>
+</div>
+
+</html>
